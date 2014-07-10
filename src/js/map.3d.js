ce21356d3fe7d01960d181bb399af86fb62eff40
Antonio Pierro <antonio.pierro@gmail.com>
2014-07-10 12:52:40 +0200

    addded the lab cube

--- src/js/map.3d.js
+++ src/js/map.3d.js
@@ -26,6 +26,10 @@
 		this.cube = this.getCube(this.cubeVertex);
 		this.cube2 = this.getCube(this.cubeVertex);
 		this.cube3 = this.getCube(this.cubeVertex);
+		this.cube4 = this.getCube(this.cubeVertex * 0.4, {
+			opacity: 0.9,
+			color: 'red'
+		});
 		this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
 	};
 
@@ -35,20 +39,46 @@
 
 		init: function () {
 			this.setSize();
-			$('body').append(this.renderer.domElement);
 			this.addObjects();
 			this.setAxisHelper();
 			this.setPosiotions();
 			this.animate();
 			window.t = this;
+			window.building = this;
+			this.renderer.domElement.addEventListener( 'click', function() {console.log(this)}, false );
 		},
 
 		addObjects: function () {
+			$('body').append(this.renderer.domElement);
 			this.scene.add(this.plane);
 			this.scene.add(this.cube);
 			this.scene.add(this.cube2);
 			this.scene.add(this.cube3);
-			this.addStairs(this.cubeVertex);
+			this.scene.add(this.cube4);
+			new THREE.Stair(this.scene, {
+				x: this.cubeVertex / 2,
+				z: -this.cubeVertex / 2,
+				y: 0,
+				height: this.floorHeight - 2
+			});
+			new THREE.Stair(this.scene, {
+				x: -this.cubeVertex / 2,
+				z: -this.cubeVertex / 2,
+				y:0,
+				height: this.floorHeight - 2
+			});
+			new THREE.Stair(this.scene, {
+				x: -this.cubeVertex / 2,
+				z: -this.cubeVertex / 2,
+				y: this.floorHeight,
+				height: this.floorHeight * 2
+			});
+			new THREE.Stair(this.scene, {
+				x: this.cubeVertex / 2,
+				z: -this.cubeVertex / 2,
+				y: this.floorHeight,
+				height: this.floorHeight * 2
+			});
 		},
 
 		animate: function () {
@@ -60,13 +90,15 @@
 			return new THREE.PerspectiveCamera(60, this.innerWidth / this.innerHeight, 0.1, 20000 );
 		},
 
-		getCube: function (vertex) {
+		getCube: function (vertex, material) {
+			var material = material || {
+				color: '#' + Math.floor(Math.random()*16777215).toString(16),
+				opacity: 0.5,
+				transparent: true
+			};
 			return new THREE.Mesh(
 				new THREE.BoxGeometry(vertex, this.floorHeight, vertex),
-				new THREE.LineBasicMaterial({
-					color: '#' + Math.floor(Math.random()*16777215).toString(16),
-					opacity: 0.5,
-					transparent:true })
+				new THREE.LineBasicMaterial(material)
 			);
 		},
 
@@ -92,50 +124,6 @@
 			return new THREE.Scene();
 		},
 
-		addStairs: function (xPosition, yPosition) {
-			// MATERIALS
-			var stepMaterialVertical = new THREE.MeshLambertMaterial({color: 0xC0C0C0});
-			var stepMaterialHorizontal = new THREE.MeshLambertMaterial({color: 0xC0C0C0});
-
-			var stepWidth = 5;
-			var stepSize = 2;
-			var stepThickness = 0.5;
-			// height from top of one step to bottom of next step up
-			var verticalStepHeight = stepSize;
-			var horizontalStepDepth = stepSize*2;
-
-			var stepHalfThickness = stepThickness/2;
-
-			// +Y direction is up
-			// Define the two pieces of the step, vertical and horizontal
-			// THREE.CubeGeometry takes (width, height, depth)
-			var stepVertical = new THREE.BoxGeometry(stepWidth, verticalStepHeight, stepThickness);
-			var stepHorizontal = new THREE.BoxGeometry(stepWidth, stepThickness, horizontalStepDepth);
-			var stepMesh;
-
-			for(var s = 0; s < 7; ++s){
-				var y = (verticalStepHeight + stepThickness) * s + verticalStepHeight/2;
-				var z = (horizontalStepDepth - stepThickness) * s;
-				// Make and position the vertical part of the step
-				stepMesh = new THREE.Mesh( stepVertical, stepMaterialVertical );
-				// The position is where the center of the block will be put.
-				// You can define position as THREE.Vector3(x, y, z) or in the following way:
-				stepMesh.position.x = xPosition || 0;            // centered at origin
-				stepMesh.position.y = yPosition || y;    // half of height: put it above ground plane
-				stepMesh.position.z = z;            // centered at origin
-				this.scene.add( stepMesh );
-
-				// Make and position the horizontal part
-				stepMesh = new THREE.Mesh( stepHorizontal, stepMaterialHorizontal );
-				stepMesh.position.x = xPosition || 0;
-				// Push up by half of horizontal step's height, plus vertical step's height
-				stepMesh.position.y = yPosition || y + (stepThickness/2 + verticalStepHeight/2);
-				// Push step forward by half the depth, minus half the vertical step's thickness
-				stepMesh.position.z = z + (horizontalStepDepth/2 - stepThickness/2);
-				this.scene.add( stepMesh );
-			}
-		},
-
 		/*
 		 * An axis object to visualize the the 3 axes in a simple way.
 		 * The X axis is red. The Y axis is green. The Z axis is blue.
@@ -150,6 +138,9 @@
 			this.cube.position.y = this.floorHeight * 0.5;
 			this.cube2.position.y = this.floorHeight * 1.5;
 			this.cube3.position.y = this.floorHeight * 2.5;
+			this.cube4.position.y = this.floorHeight * 2.5;
+			this.cube4.position.x = this.cubeVertex / 3;
+			this.cube4.position.z = this.cubeVertex / 3.5;
 		},
 
 		setSize: function () {
--- src/js/threeStairs.js
+++ src/js/threeStairs.js
@@ -4,11 +4,13 @@ THREE.Stair = function (scene, geometry) {
     var stepMaterialHorizontal = new THREE.MeshLambertMaterial({color: 0xC0C0C0});
 
     var stepWidth = 5;
-    var stepSize = 2;
+    var stepSize = 1;
     var stepThickness = 0.5;
     // height from top of one step to bottom of next step up
     var verticalStepHeight = stepSize;
     var horizontalStepDepth = stepSize*2;
+    // height limit before changing the direction of a staircase
+    var heightBeforeChangeDirection = geometry.height / 2;
 
     var stepHalfThickness = stepThickness/2;
 
@@ -18,16 +20,17 @@ THREE.Stair = function (scene, geometry) {
     var stepVertical = new THREE.BoxGeometry(stepWidth, verticalStepHeight, stepThickness);
     var stepHorizontal = new THREE.BoxGeometry(stepWidth, stepThickness, horizontalStepDepth);
     var stepMesh;
+    var y = 0, z = geometry.z || 0 ;
 
-    for (var s = 0; s < 7; ++s) {
-        var y = (verticalStepHeight + stepThickness) * s + verticalStepHeight / 2;
-        var z = (horizontalStepDepth - stepThickness) * s;
+    for (var s = 0; y < geometry.height; ++s) {
+        y = (verticalStepHeight + stepThickness) * s + verticalStepHeight / 2 + geometry.y;
+        z = (horizontalStepDepth - stepThickness) * s + geometry.z;
         // Make and position the vertical part of the step
         stepMesh = new THREE.Mesh( stepVertical, stepMaterialVertical );
         // The position is where the center of the block will be put.
         // You can define position as THREE.Vector3(x, y, z) or in the following way:
         stepMesh.position.x = geometry.x || 0;            // centered at origin
-        stepMesh.position.y = - geometry.y || y;    // half of height: put it above ground plane
+        stepMesh.position.y = y;    // half of height: put it above ground plane
         stepMesh.position.z = z;            // centered at origin
         scene.add( stepMesh );
 
@@ -35,7 +38,7 @@ THREE.Stair = function (scene, geometry) {
         stepMesh = new THREE.Mesh( stepHorizontal, stepMaterialHorizontal );
         stepMesh.position.x = geometry.x || 0;
         // Push up by half of horizontal step's height, plus vertical step's height
-        stepMesh.position.y = geometry.y || y + (stepThickness/2 + verticalStepHeight/2);
+        stepMesh.position.y = (stepThickness/2 + verticalStepHeight/2) + y;
         // Push step forward by half the depth, minus half the vertical step's thickness
         stepMesh.position.z = z + (horizontalStepDepth/2 - stepThickness/2);
         scene.add( stepMesh );
