76b2cbfbefca62f6fd9437f520f1256996ec688a
Antonio Pierro <antonio.pierro@gmail.com>
2014-07-10 09:00:51 +0200

    add library to add a stair

---
+++ src/js/threeStairs.js
@@ -0,0 +1,43 @@
+THREE.Stair = function (scene, geometry) {
+    // MATERIALS
+    var stepMaterialVertical = new THREE.MeshLambertMaterial({color: 0xC0C0C0});
+    var stepMaterialHorizontal = new THREE.MeshLambertMaterial({color: 0xC0C0C0});
+
+    var stepWidth = 5;
+    var stepSize = 2;
+    var stepThickness = 0.5;
+    // height from top of one step to bottom of next step up
+    var verticalStepHeight = stepSize;
+    var horizontalStepDepth = stepSize*2;
+
+    var stepHalfThickness = stepThickness/2;
+
+    // +Y direction is up
+    // Define the two pieces of the step, vertical and horizontal
+    // THREE.CubeGeometry takes (width, height, depth)
+    var stepVertical = new THREE.BoxGeometry(stepWidth, verticalStepHeight, stepThickness);
+    var stepHorizontal = new THREE.BoxGeometry(stepWidth, stepThickness, horizontalStepDepth);
+    var stepMesh;
+
+    for (var s = 0; s < 7; ++s) {
+        var y = (verticalStepHeight + stepThickness) * s + verticalStepHeight / 2;
+        var z = (horizontalStepDepth - stepThickness) * s;
+        // Make and position the vertical part of the step
+        stepMesh = new THREE.Mesh( stepVertical, stepMaterialVertical );
+        // The position is where the center of the block will be put.
+        // You can define position as THREE.Vector3(x, y, z) or in the following way:
+        stepMesh.position.x = geometry.x || 0;            // centered at origin
+        stepMesh.position.y = - geometry.y || y;    // half of height: put it above ground plane
+        stepMesh.position.z = z;            // centered at origin
+        scene.add( stepMesh );
+
+        // Make and position the horizontal part
+        stepMesh = new THREE.Mesh( stepHorizontal, stepMaterialHorizontal );
+        stepMesh.position.x = geometry.x || 0;
+        // Push up by half of horizontal step's height, plus vertical step's height
+        stepMesh.position.y = geometry.y || y + (stepThickness/2 + verticalStepHeight/2);
+        // Push step forward by half the depth, minus half the vertical step's thickness
+        stepMesh.position.z = z + (horizontalStepDepth/2 - stepThickness/2);
+        scene.add( stepMesh );
+    }
+}
